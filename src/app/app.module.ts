540389f2d27c9096e6dc771fcd09b89f88a609ef
bangtur_studio <dani.widodo87@gmail.com>
2022-02-03 21:53:26 +0800

    add pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { PostsComponent } from './components/pages/posts/posts.component';
+import { CategoriesComponent } from './components/pages/categories/categories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent,
+    RegisterComponent,
+    PostsComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
